155a885ba2d42f256cc069511717102922647d7a
Bumseok Cho <31695636+brianbscho@users.noreply.github.com>
2020-07-28 15:25:01 +0900

    design (CSS) settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,26 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
-      colors: {
+      textColor: {
         primary: "#FF257B",
         secondary: "#00F0FF",
         type: "#041524",
+      },
+      maxWidth: {
+        mobile: "300px",
+      },
+      width: {
+        mobile: "300px",
+      },
+      height: {
+        "1/4": "25%",
+        "9/10": "90%",
+        "8/10": "80%",
+        "6/10": "60%",
+      },
+      fontFamily: {
+        nunito: ["Nunito"],
+        "noto-sans-kr": ["Noto Sans KR"],
       },
     },
   },
